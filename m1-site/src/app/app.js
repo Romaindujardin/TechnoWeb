e32adc93794877b5a5f17b13dfb5178a4a400e9a
Hugo CORNE <hugo.corne@student.junia.com>
2024-11-08 11:08:15 +0100

    ajout du Menu et Layout

--- m1-site/src/app/app.js
+++ m1-site/src/app/app.js
@@ -1,11 +1,22 @@
+// src/app/app.js
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import BooksPage from "./pages/BooksPage";
+import AuthorsPage from "./pages/AuthorsPage";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100 p-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/books" element={<BooksPage />} />
+          <Route path="/authors" element={<AuthorsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ m1-site/src/app/pages/HomePage.js
@@ -0,0 +1,13 @@
+// src/app/pages/HomePage.js
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default HomePage;
